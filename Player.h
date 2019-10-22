7d0e33aa8670c02e06bbf78f2c04590f1b83b37a
Aaron Daugherty <acd@uga.edu>
2019-10-22 19:19:16 -0400

    Added beginning of Player class to Player.h

--- Player.h
+++ Player.h
@@ -1,1 +1,22 @@
-#in
+#ifndef
+#define
+
+class Player {
+private:
+    String name;
+    bool win;
+public:
+    Player(String name = "Zoey");
+    String getName();
+    void setName(String name);
+    void setWin(bool win);
+    bool getWin();
+
+
+
+}
+
+
+
+
+#endif
