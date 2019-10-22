3fd96934a907d1603eb5a66e7e5e8c5bd4f0b305
WILLIAM CORDER <corder@nike.cs.uga.edu>
2019-10-22 19:47:28 -0400

    humanplayer.h start

--- Player.h
+++ Player.h
@@ -1,25 +1,17 @@
-<<<<<<< HEAD
-#ifndef 
-#define
-=======
 #ifndef PLAYER_H
 #define PLAYER_H
->>>>>>> 0edf83b8f8106a96e6a3047158308cc5c55d0913
 
 class Player {
 private:
     string name;
     bool win;
 public:
-<<<<<<< HEAD
-=======
-    Player();
->>>>>>> 0edf83b8f8106a96e6a3047158308cc5c55d0913
-    string getName();
-    void setName(string name);
-    void setWin(bool win);
-    bool getWin();
-
+  Player();
+  string getName();
+  void setName(string name);
+  void setWin(bool win);
+  bool getWin();
+  
 
 
 };
